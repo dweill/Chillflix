2a1f40d4fd8a2fc58d3ad8e9e68f1f863ac4ae0a
Daniel Weill <daniel.zalmen@gmail.com>
2017-06-20 17:28:00 -0500

    Added an s in http

--- client/services/random.js
+++ client/services/random.js
@@ -2,7 +2,7 @@ angular.module('app')
 .service('Random', function($window, $http) {
   return {
     search: function(query, callback) {
-      $http.get('http://netflixroulette.net/api/api.php?', {
+      $http.get('https://netflixroulette.net/api/api.php?', {
         params: {
           title: query,
         },
