c9edb35dec8377f34985a88f3b48edb71852c88b
Daniel Weill <daniel.zalmen@gmail.com>
2017-06-19 20:07:37 -0500

    Index page and Sign up page made. Both rendering

--- server.js
+++ server.js
@@ -1,8 +1,39 @@
 /* eslint no-console: 0 */
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient();
+const path = require('path');
+require('dotenv').config();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('I am working');
+const PORT = process.env.PORT;
+const MONGOURI = process.env.MONGO_URI;
+let db;
+MongoClient.connect(MONGOURI, (err, database) => {
+  if (err) return console.log(err);
+  db = database;
+  return app.listen(PORT, () => {
+    console.log('I am working');
+  });
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/+json' }));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, '/signup.html'));
+});
+
+app.post('/signup', (req, res) => {
+  console.log(req.body, 'hey');
+  db.connect('users').save(req.body, (err, results) => {
+    if (err) return console.log(err);
+    console.log('Account Created!');
+    res.redirect('/');
+  });
 });
