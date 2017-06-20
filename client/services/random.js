7dac05e230ce512d60ab2897b46ed45733fcf3a9
Daniel Weill <daniel.zalmen@gmail.com>
2017-06-20 17:37:02 -0500

    Testing path in chrome

--- client/services/random.js
+++ client/services/random.js
@@ -2,7 +2,7 @@ angular.module('app')
 .service('Random', function($window, $http) {
   return {
     search: function(query, callback) {
-      $http.get('https://netflixroulette.net/api/api.php?', {
+      $http.get('//netflixroulette.net/api/api.php?', {
         params: {
           title: query,
         },
